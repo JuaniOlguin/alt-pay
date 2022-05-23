d1f83b08ee12f8ca84c61f3760a40e2dba168122
JuaniOlguin <juani.olguin@gmail.com>
2022-05-23 09:47:42 -0300

    Cambios done

--- js/global.js
+++ js/global.js
@@ -1,5 +1,3 @@
-
-
 if(document.getElementById('backdownDiv')){
     const TimeOutBackDownDiv = setTimeout(displayNone, 2000);
 }
@@ -7,4 +5,16 @@ if(document.getElementById('backdownDiv')){
 function displayNone(){
     let backdownDiv = document.getElementById('backdownDiv')
     backdownDiv.classList.add("hidden");
+}
+function copyToClipboard() {
+    /* Get the text field */
+    var copyText = document.getElementById("copyText");
+    var btnTooltipCopy = document.getElementById("btnTooltipCopy");
+    
+    /* Select the text field */
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); /* For mobile devices */
+  
+     /* Copy the text inside the text field */
+    navigator.clipboard.writeText(copyText.value);
 }
